11492713572fbc8210761e6f9ea80c6a5f2d7cb3
amyeo-maqai <amyeo.info@gmail.com>
2023-03-17 18:47:40 +0530

    new styles

--- insurancebase/static/js/index.js
+++ insurancebase/static/js/index.js
@@ -1,27 +1,40 @@
-function postFile(event) {
-    event.preventDefault();
-    const input = document.getElementById('file-input');
-    const file = input.files[0];
-    const fileType=file.type;
-    const formData = new FormData();
-    formData.append('file', file);
-    const options = {
-      method: 'POST',
-      body: formData
-    };
-    fetch('http://3.139.167.247:5000/', options)
-      .then(response => response)
-      .then(response => {
-        console.log(response)
+let postFile=async (event)=> {
+  event.preventDefault();
+  const input = document.getElementById('file-input');
+  const file = input.files[0];
+  const fileType = file.type;
+  const formData = new FormData();
+  let spinnerEl = document.getElementById('spinner');
+  spinnerEl.classList.remove("d-none");
+  formData.append('file', file);
+  const options = {
+    method: 'POST',
+    body: formData
+  };
+
+  try {
+    const response=await fetch('http://3.139.167.247:5000/fileupload/', options)
+      .then(response => response.json())
+      .then(data => {
+        console.log(typeof data)
+        // console.log(response.json())
         const output = document.getElementById('output');
-        if (response.status===200){
-        output.textContent ="Status:"+ response.status+" a "+fileType+"  is uploaded";
+        spinnerEl.classList.add("d-none");
+        if (data.status === 200) {
+          output.textContent = "Result:" + data.message
         }
-        else if (file===undefined){
-            output.textContent="Please select a file"
+        else {
+          output.textContent = "Please select a file"
         }
       })
       .catch(error => {
-        console.error('Error uploading file:', error);
+        // console.log(error)
+        // console.log(error.message)
+        spinnerEl.classList.add("d-none");
+        output.textContent = "Failed To Upload"
       });
   }
+  catch(error){
+    console.log(error.message)
+  }
+}
